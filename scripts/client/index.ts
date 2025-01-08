540181727db01bde8d1a3bf88593ad6cdc76d9e1
juji <him@jujiyangasli.com>
2025-01-08 11:03:59 +0700

    check existing client

--- scripts/client/index.ts
+++ scripts/client/index.ts
@@ -1,22 +1,36 @@
 import { text, log, isCancel } from '@clack/prompts';
+import { stat } from 'fs/promises';
 import { writeFile } from 'fs/promises'
 
 export default async function client (){
 
-  const cc = await text({
-    message: "What is the client code you want to use?",
-    placeholder: 'smallcase.nospace',
-    validate(value) {
-      if (value.length === 0) return `Value is required!`;
-    },
-  });
+  let code = ''
 
-  if(isCancel(cc)){
-    log.error('canceled')
-    return;
+  while(!code){
+
+    const cc = await text({
+      message: "What is the client code you want to use?",
+      placeholder: 'smallcase nospace nosymbols dot.allowed alphabeth.first',
+      validate(value) {
+        if (value.length === 0) return `Value is required!`;
+        if(!value.match(/^[a-z]+([a-z0-9.])+$/)) return 'smallcase nospace nosymbols dot.allowed alphabeth.first'
+      },
+    });
+  
+    if(isCancel(cc)){
+      log.error('canceled')
+      return;
+    }
+  
+    code = cc.valueOf().toString().toLowerCase().replace(/[\s\r\n]/g,'')
+
+    try{
+      await stat(`./scripts/data/client/${code}.json`)
+      code = ''
+      log.error(`the code '${code}' has been used`)
+    }catch(e){}
+
   }
-
-  const code = cc.valueOf().toString().toLowerCase().replace(/[\s\r\n]/g,'')
 
   const n = await text({
     message: "What is the client company name?",
